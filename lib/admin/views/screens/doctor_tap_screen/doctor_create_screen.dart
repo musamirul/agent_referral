b7bc70b8eae1868a61c9943147cd09eddcff1058
ameirul mustaqim <musamirul.kpj@gmail.com>
2024-08-05 01:40:36 +0800

    create doctor screen

--- lib/admin/views/screens/doctor_tap_screen/doctor_create_screen.dart
+++ lib/admin/views/screens/doctor_tap_screen/doctor_create_screen.dart
@@ -1,10 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
-class DoctorCreateScreen extends StatelessWidget {
+class DoctorCreateScreen extends StatefulWidget {
   const DoctorCreateScreen({super.key});
 
   @override
+  State<DoctorCreateScreen> createState() => _DoctorCreateScreenState();
+}
+
+class _DoctorCreateScreenState extends State<DoctorCreateScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  TextEditingController doctorName = TextEditingController();
+  TextEditingController doctorSpeciality = TextEditingController();
+  String? type;
+
+  List<String> _typeOption = [
+    'Visiting','Resident','Sessional'
+  ];
+
+  void saveConsultant() async{
+    if(_formKey.currentState!.validate()){
+      final doctorId = Uuid().v4();
+      await _firestore.collection('doctors').doc(doctorId).set({
+        'name' : doctorName.text,
+        'speciality' : doctorSpeciality.text,
+        'type' : type,
+      }).whenComplete(() {
+        doctorName.clear();
+        doctorSpeciality.clear();
+      },);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Doctor Create Screen')),);
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Scaffold(
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return 'Please Enter Doctor Name';
+                    }else{
+                      return null;
+                    }
+                  },
+                  controller: doctorName,
+                  decoration: InputDecoration(label: Text('Doctor Name')),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return 'Please Enter Doctor Speciality';
+                    }else{
+                      return null;
+                    }
+                  },
+                  controller: doctorSpeciality,
+                  decoration: InputDecoration(label: Text('Doctor Speciality')),
+                  maxLines: 2,
+                ),
+                SizedBox(height: 20,),
+                DropdownButtonFormField(hint: Text('Select Doctor Residency'),items: _typeOption.map<DropdownMenuItem<String>>((value) {
+                  return DropdownMenuItem(child: Text(value),value: value,);
+                }).toList(), onChanged: (value) {
+                  type = value;
+                },),
+                SizedBox(height: 20,),
+                ElevatedButton.icon(onPressed: () {
+                  saveConsultant();
+                }, label: Text('Add Doctor'),
+                icon: Icon(Icons.add),),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/admin/views/screens/doctor_tap_screen/doctor_list_screen.dart
+++ lib/admin/views/screens/doctor_tap_screen/doctor_list_screen.dart
@@ -1,10 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DoctorListScreen extends StatelessWidget {
+class DoctorListScreen extends StatefulWidget {
   const DoctorListScreen({super.key});
 
   @override
+  State<DoctorListScreen> createState() => _DoctorInfoScreenState();
+}
+
+class _DoctorInfoScreenState extends State<DoctorListScreen> {
+  List<dynamic> _allResults = [];
+  List<dynamic> _resultList = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getClientStream();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  _onSearchChanged() {
+    searchResultList();
+  }
+
+  searchResultList() {
+    List<dynamic> showResults = [];
+    if (_searchController.text != "") {
+      for (var clientSnapshot in _allResults) {
+        var name = clientSnapshot['name'].toString().toLowerCase();
+        if (name.contains(_searchController.text.toLowerCase())) {
+          showResults.add(clientSnapshot);
+        }
+      }
+    } else {
+      showResults = List.from(_allResults);
+    }
+    setState(() {
+      _resultList = showResults;
+    });
+  }
+
+  getClientStream() async {
+    var data = await FirebaseFirestore.instance
+        .collection('doctors')
+        .orderBy('name')
+        .get();
+    setState(() {
+      _allResults = data.docs;
+      searchResultList();
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Doctor List Screen')),);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: CupertinoSearchTextField(
+          controller: _searchController,
+        ),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: _resultList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_resultList[index]['name']),
+            subtitle: Text(
+              _resultList[index]['speciality'],
+              style: TextStyle(fontSize: 12),
+            ),
+            trailing: Text(_resultList[index]['type']),
+          );
+        },
+      ),
+    );
   }
 }
