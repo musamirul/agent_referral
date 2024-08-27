069e9259c78991fc984f5aba0dd02f1bb812767a
ameirul mustaqim <46254149+musamirul@users.noreply.github.com>
2024-08-27 17:31:09 +0800

    update facility

--- lib/agent/views/widgets/facility_widget.dart
+++ lib/agent/views/widgets/facility_widget.dart
@@ -5,6 +5,6 @@ class FacilityWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(padding: EdgeInsets.all(10));
   }
 }
