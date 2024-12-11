8f29f8ebfae99c5699082117932d6292e276f6e2
ameirul mustaqim <musamirul.kpj@gmail.com>
2024-12-12 02:24:56 +0800

    update setting to latest sdk
    sdk, java

--- android/app/src/main/java/io/flutter/app/FlutterMultiDexApplication.java
+++ android/app/src/main/java/io/flutter/app/FlutterMultiDexApplication.java
@@ -5,21 +5,21 @@
 // Modifications to this file should be done in a copy under a different name
 // as this file may be regenerated.
 
-package io.flutter.app;
-
-import android.app.Application;
-import android.content.Context;
-import androidx.annotation.CallSuper;
-import androidx.multidex.MultiDex;
-
-/**
- * Extension of {@link android.app.Application}, adding multidex support.
- */
-public class FlutterMultiDexApplication extends Application {
-  @Override
-  @CallSuper
-  protected void attachBaseContext(Context base) {
-    super.attachBaseContext(base);
-    MultiDex.install(this);
-  }
-}
+//package io.flutter.app;
+//
+//import android.app.Application;
+//import android.content.Context;
+//import androidx.annotation.CallSuper;
+//import androidx.multidex.MultiDex;
+//
+///**
+// * Extension of {@link android.app.Application}, adding multidex support.
+// */
+//public class FlutterMultiDexApplication extends Application {
+//  @Override
+//  @CallSuper
+//  protected void attachBaseContext(Context base) {
+//    super.attachBaseContext(base);
+//    MultiDex.install(this);
+//  }
+//}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,9 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
+import open_file_mac
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
