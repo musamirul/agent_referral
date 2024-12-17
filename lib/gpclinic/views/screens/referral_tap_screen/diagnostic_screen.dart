e6e9b087bad30102e018ba07fed14deaa3422ee4
ameirul mustaqim <musamirul.kpj@gmail.com>
2024-12-18 00:53:28 +0800

    add controller and screen diagnostic

--- lib/gpclinic/views/screens/referral_tap_screen/diagnostic_screen.dart
+++ lib/gpclinic/views/screens/referral_tap_screen/diagnostic_screen.dart
@@ -9,6 +9,7 @@ class DiagnosticScreen extends StatefulWidget {
 }
 
 class _DiagnosticScreenState extends State<DiagnosticScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   List<String> _sexOption = ['Male', 'Female', 'Others'];
   String? sex;
@@ -16,104 +17,76 @@ class _DiagnosticScreenState extends State<DiagnosticScreen> {
   List<String> _nationalityOption = ['Malaysian', 'Foreigner', 'Others'];
   String? nationality;
 
+  late String name;
+  late String identity;
+  late String phone;
+  late String address;
+
+  _saveDiagnosticReferral() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: Padding(
-        padding: const EdgeInsets.only(left: 20,right: 20,bottom: 70),
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 70),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 20,),
-              Text('Patient Information', style: GoogleFonts.poppins(
-              fontWeight: FontWeight.w700,
-                  fontSize: 15,
-                  letterSpacing: 0.5,
-                  ),),
-              SizedBox(height: 5,),
-              TextFormField(
-                validator: (value) {
-                  if(value!.isEmpty){
-                    return "Enter Name";
-                  }else{
-                    return null;
-                  }
-                },
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Patient Information',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15,
+                    letterSpacing: 0.5,
                   ),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: 'Patient Name',
-                  labelStyle: GoogleFonts.getFont("Nunito Sans",
-                      fontSize: 14, letterSpacing: 0.1),
                 ),
-                textInputAction: TextInputAction.next,
-              ),
-              SizedBox(height: 10,),
-              TextFormField(
-                validator: (value) {
-                  if(value!.isEmpty){
-                    return 'Enter I/C or Passport Number';
-                  }else{
-                    return null;
-                  }
-                },
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: 'IC or Passport Number',
-                  labelStyle: GoogleFonts.getFont("Nunito Sans",
-                      fontSize: 14, letterSpacing: 0.1),
+                SizedBox(
+                  height: 5,
                 ),
-                textInputAction: TextInputAction.next,
-              ),
-          
-              SizedBox(height: 10,),
-              DropdownButtonFormField<String>(
-                hint: Text('Select Nationality'),
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelStyle: GoogleFonts.getFont("Nunito Sans",
-                      fontSize: 14, letterSpacing: 0.1),
-                ),
-                items: _nationalityOption.map<DropdownMenuItem<String>>(
-                      (String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    nationality = value;
-                  });
-                },
-              ),
-              if (nationality == 'Foreigner' || nationality == 'Others')
                 TextFormField(
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Enter Nationality';
+                      return "Enter Name";
                     } else {
                       return null;
                     }
+                  },
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'Patient Name',
+                    labelStyle: GoogleFonts.getFont("Nunito Sans",
+                        fontSize: 14, letterSpacing: 0.1),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter I/C or Passport Number';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    identity = value;
                   },
                   decoration: InputDecoration(
                     fillColor: Colors.white,
@@ -122,94 +95,163 @@ class _DiagnosticScreenState extends State<DiagnosticScreen> {
                         borderRadius: BorderRadius.circular(10)),
                     focusedBorder: InputBorder.none,
                     enabledBorder: InputBorder.none,
-                    labelText: 'Enter Nationality',
+                    labelText: 'IC or Passport Number',
                     labelStyle: GoogleFonts.getFont("Nunito Sans",
                         fontSize: 14, letterSpacing: 0.1),
                   ),
                   textInputAction: TextInputAction.next,
                 ),
-          
-          
-              SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: Icon(Icons.house),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: 'Home Address',
-                  labelStyle: GoogleFonts.getFont("Nunito Sans",
-                      fontSize: 14, letterSpacing: 0.1),
+                SizedBox(
+                  height: 10,
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter Home Address';
-                  } else {
-                    return null;
-                  }
-                },
-                maxLength: 800,
-                maxLines: 3,
-                textInputAction: TextInputAction.next,
-              ),
-          
-          
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter Contact Number';
-                  } else {
-                    return null;
-                  }
-                },
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: 'Contact Number',
-                  prefixIcon: Icon(Icons.phone_android),
-                  labelStyle: GoogleFonts.getFont("Nunito Sans",
-                      fontSize: 14, letterSpacing: 0.1),
-                ),
-                textInputAction: TextInputAction.next,
-              ),
-          
-              SizedBox(height: 10,),
-              DropdownButtonFormField<String>(
-                hint: Text('Select Gender'),
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: 'Gender',
-                  labelStyle: GoogleFonts.getFont("Nunito Sans",
-                      fontSize: 14, letterSpacing: 0.1),
-                ),
-                items: _sexOption.map<DropdownMenuItem<String>>(
-                      (String value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
+                DropdownButtonFormField<String>(
+                  hint: Text('Select Nationality'),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelStyle: GoogleFonts.getFont("Nunito Sans",
+                        fontSize: 14, letterSpacing: 0.1),
+                  ),
+                  items: _nationalityOption.map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      nationality = value;
+                    });
                   },
-                ).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    sex = value;
-                  });
-                },
-              ),
-          
-            ],
+                ),
+                if (nationality == 'Foreigner' || nationality == 'Others')
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter Nationality';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: 'Enter Nationality',
+                      labelStyle: GoogleFonts.getFont("Nunito Sans",
+                          fontSize: 14, letterSpacing: 0.1),
+                    ),
+                    textInputAction: TextInputAction.next,
+                  ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: Icon(Icons.house),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'Home Address',
+                    labelStyle: GoogleFonts.getFont("Nunito Sans",
+                        fontSize: 14, letterSpacing: 0.1),
+                  ),
+                  onChanged: (value) {
+                    address = value;
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter Home Address';
+                    } else {
+                      return null;
+                    }
+                  },
+                  maxLength: 800,
+                  maxLines: 3,
+                  textInputAction: TextInputAction.next,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter Contact Number';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    phone = value;
+                  },
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'Contact Number',
+                    prefixIcon: Icon(Icons.phone_android),
+                    labelStyle: GoogleFonts.getFont("Nunito Sans",
+                        fontSize: 14, letterSpacing: 0.1),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                DropdownButtonFormField<String>(
+                  hint: Text('Select Gender'),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'Gender',
+                    labelStyle: GoogleFonts.getFont("Nunito Sans",
+                        fontSize: 14, letterSpacing: 0.1),
+                  ),
+                  items: _sexOption.map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      sex = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    fixedSize: Size(300, 30),
+                  ),
+                    onPressed: () {
+                      _saveDiagnosticReferral();
+                    },
+                    label: Text(
+                      'Add Referral',
+                      style: TextStyle(color: Colors.white),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
